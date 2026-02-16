f1f9737c808c2e6e3310c3557320097cfded3079
ask7th <sashapopova@icloud.com>
2026-02-16 12:24:27 +0200

    added checks in total_salary

--- first/main.py
+++ first/main.py
@@ -1,30 +1,39 @@
 def total_salary(path) :
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            salaries_list = file.readlines()
+            salaries = []
+            for line in salaries_list:
+                parts = line.strip().split(",")
+                if len(parts) != 2:
+                    return("File is corrupted")
+                                  
+                try:
+                    salaries.append(float(parts[1]))
+                except ValueError:
+                    return("File is corrupted")                
+                
+            if not salaries:
+                return 0, 0
+            
+            total = sum(salaries)
+            avg_salary = total / len(salaries)
+            return total, avg_salary
+    
+    except FileNotFoundError:
+        return("File not found")
+         
+    except UnicodeDecodeError:
+        return("File is corrupted")
+     
 
-    with open(path, "r", encoding="utf-8") as file:
-        salaries_list = file.readlines()
-        salaries = []
-        for line in salaries_list:
-            parts = line.strip().split(",")
-            if len(parts) != 2:
-                continue
-            try:
-                salaries.append(float(parts[1]))
-            except ValueError:
-                continue
-        if not salaries:
-            return 0, 0
-        
-        total = sum(salaries)
-        avg_salary = total / len(salaries)
-        return total, avg_salary
+result = total_salary("salary.txt")
+if isinstance(result, tuple):
+    total, average = result
+    print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+else:
+    print(result)
 
 
-total, average = total_salary("salary.txt")
-print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
-
-
-print(f"\nFor wrong path:")
-try:
-    total, average = total_salary("salarys.txt")
-except FileNotFoundError as e:
-    print(f"File not found: {e.filename}")
+assert total_salary("salarys.txt") == "File not found"
+assert total_salary("err_data.txt") == "File is corrupted"
